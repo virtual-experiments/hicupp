96c558328534f9ec35ac407b18c3caaf1e33f4b4
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-16 01:40:59 +0200

    GitHub Actions workflow for building

---
+++ hicupp/build-macos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+GIF_FILES=($(cd src; find . -name \*.gif))
+javac -d bin --module-source-path hicupp=src -m hicupp
+for F in $GIF_FILES
+do
+  cp src/$F bin/hicupp/$F
+done
+jpackage -p bin -m hicupp/interactivehicupp.ImagesMain --name HicuppForImages --type app-image # --mac-sign
+# zip -r HicuppForImages-macos-unstapled.zip HicuppForImages.app
+# xcrun notarytool submit HicuppForImages-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
+# xcrun stapler staple HicuppForImages.app
+zip -r HicuppForImages-macos.zip HicuppForImages.app
+jpackage -p bin -m hicupp/interactivehicupp.GeneralMain --name HicuppForGeneralPointSets --type app-image # --mac-sign
+# zip -r HicuppForGeneralPointSets-macos-unstapled.zip HicuppForGeneralPointSets.app
+# xcrun notarytool submit HicuppForGeneralPointSets-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
+# xcrun stapler staple HicuppForGeneralPointSets.app
+zip -r HicuppForGeneralPointSets-macos.zip HicuppForGeneralPointSets.app
+jpackage -p bin -m hicupp/hicupp.SequentialHicupp --name SequentialHicupp --type app-image # --mac-sign
+# zip -r SequentialHicupp-macos-unstapled.zip SequentialHicupp.app
+# xcrun notarytool submit SequentialHicupp--macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
+# xcrun stapler staple SequentialHicupo.app
+zip -r SequentialHicupp-macos.zip SequentialHicupp.app
