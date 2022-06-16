ec2ea1b6b9f936ece10f8debce0f287c405e15d1
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-16 09:17:12 +0200

    Add missing launchers for Linux

---
+++ hicupp/run-SequentialHicupp.c
@@ -0,0 +1,20 @@
+// Launcher for the Linux release
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+char targetCmd[] = "bin/SequentialHicupp";
+
+int main(int argc, char **argv) {
+//  printf("argv[0] == \"%s\"\n", argv[0]);
+  int commandLength = strlen(argv[0]) + 50;
+  char *command = malloc(commandLength);
+  strcpy(command, argv[0]);
+  char *lastSlash = strrchr(command, '/');
+  if (lastSlash == NULL)
+    strcpy(command, targetCmd);
+  else
+    strcpy(lastSlash + 1, targetCmd);
+  return system(command);
+}
