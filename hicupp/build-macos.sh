8eb426a9729a9138cf06a467844f87b1c7566881
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-16 10:09:10 +0200

    MacOS: Use ditto instead of zip to create .zips
    Hopefully fixes the notarization failure

--- hicupp/build-macos.sh
+++ hicupp/build-macos.sh
@@ -8,22 +8,22 @@ do
 done
 
 jpackage -p bin -m hicupp/interactivehicupp.ImagesMain --name HicuppForImages --type app-image --mac-sign
-zip -r HicuppForImages-macos-unstapled.zip HicuppForImages.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForImages-macos-unstapled.zip HicuppForImages.app
 xcrun notarytool submit HicuppForImages-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
 rm HicuppForImages-macos-unstapled.zip
 xcrun stapler staple HicuppForImages.app
-zip -r HicuppForImages-macos.zip HicuppForImages.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForImages-macos.zip HicuppForImages.app
 
 jpackage -p bin -m hicupp/interactivehicupp.GeneralMain --name HicuppForGeneralPointSets --type app-image --mac-sign
-zip -r HicuppForGeneralPointSets-macos-unstapled.zip HicuppForGeneralPointSets.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForGeneralPointSets-macos-unstapled.zip HicuppForGeneralPointSets.app
 xcrun notarytool submit HicuppForGeneralPointSets-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
 rm HicuppForGeneralPointSets-macos-unstapled.zip
 xcrun stapler staple HicuppForGeneralPointSets.app
-zip -r HicuppForGeneralPointSets-macos.zip HicuppForGeneralPointSets.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForGeneralPointSets-macos.zip HicuppForGeneralPointSets.app
 
 jpackage -p bin -m hicupp/hicupp.SequentialHicupp --name SequentialHicupp --type app-image --mac-sign
-zip -r SequentialHicupp-macos-unstapled.zip SequentialHicupp.app
+ditto -c -k --keepParent --sequesterRsrc SequentialHicupp-macos-unstapled.zip SequentialHicupp.app
 xcrun notarytool submit SequentialHicupp--macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
 rm SequentialHicupp-macos-unstapled.zip
 xcrun stapler staple SequentialHicupo.app
-zip -r SequentialHicupp-macos.zip SequentialHicupp.app
+ditto -c -k --keepParent --sequesterRsrc SequentialHicupp-macos.zip SequentialHicupp.app
