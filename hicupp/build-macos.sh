ed0ee4816f2e240846bc15d8d0d2caf8b8d16537
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2022-06-16 10:22:14 +0200

    Fix order of ditto arguments

--- hicupp/build-macos.sh
+++ hicupp/build-macos.sh
@@ -8,22 +8,22 @@ do
 done
 
 jpackage -p bin -m hicupp/interactivehicupp.ImagesMain --name HicuppForImages --type app-image --mac-sign
-ditto -c -k --keepParent --sequesterRsrc HicuppForImages-macos-unstapled.zip HicuppForImages.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForImages.app HicuppForImages-macos-unstapled.zip
 xcrun notarytool submit HicuppForImages-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
 rm HicuppForImages-macos-unstapled.zip
 xcrun stapler staple HicuppForImages.app
-ditto -c -k --keepParent --sequesterRsrc HicuppForImages-macos.zip HicuppForImages.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForImages.app HicuppForImages-macos.zip
 
 jpackage -p bin -m hicupp/interactivehicupp.GeneralMain --name HicuppForGeneralPointSets --type app-image --mac-sign
-ditto -c -k --keepParent --sequesterRsrc HicuppForGeneralPointSets-macos-unstapled.zip HicuppForGeneralPointSets.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForGeneralPointSets.app HicuppForGeneralPointSets-macos-unstapled.zip
 xcrun notarytool submit HicuppForGeneralPointSets-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
 rm HicuppForGeneralPointSets-macos-unstapled.zip
 xcrun stapler staple HicuppForGeneralPointSets.app
-ditto -c -k --keepParent --sequesterRsrc HicuppForGeneralPointSets-macos.zip HicuppForGeneralPointSets.app
+ditto -c -k --keepParent --sequesterRsrc HicuppForGeneralPointSets.app HicuppForGeneralPointSets-macos.zip
 
 jpackage -p bin -m hicupp/hicupp.SequentialHicupp --name SequentialHicupp --type app-image --mac-sign
-ditto -c -k --keepParent --sequesterRsrc SequentialHicupp-macos-unstapled.zip SequentialHicupp.app
-xcrun notarytool submit SequentialHicupp--macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
+ditto -c -k --keepParent --sequesterRsrc SequentialHicupp.app SequentialHicupp-macos-unstapled.zip
+xcrun notarytool submit SequentialHicupp-macos-unstapled.zip --keychain-profile APPLE_ID_PASSWORD --wait
 rm SequentialHicupp-macos-unstapled.zip
 xcrun stapler staple SequentialHicupo.app
-ditto -c -k --keepParent --sequesterRsrc SequentialHicupp-macos.zip SequentialHicupp.app
+ditto -c -k --keepParent --sequesterRsrc SequentialHicupp.app SequentialHicupp-macos.zip
